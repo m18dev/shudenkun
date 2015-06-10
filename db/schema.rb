dd82d04841a2a01cbbcb4228f1bd8a9379a08bd7
Kota Kubo <ko-kubo@m3.com>
2015-06-10 21:30:51 +0900

    api取得部分の実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150610115404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "routes", force: :cascade do |t|
+    t.time     "limit_time"
+    t.string   "station_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.float    "x"
+    t.float    "y"
+    t.string   "company_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
