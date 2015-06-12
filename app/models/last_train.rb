01c465e2802237235fe4e62eb9236d1663315e33
t-murata <ta-murata@m3.com>
2015-06-12 16:10:43 +0900

    終電情報を確保。

--- app/models/last_train.rb
+++ app/models/last_train.rb
@@ -1,3 +1,14 @@
+require 'spider_web'
+
 class LastTrain < ActiveRecord::Base
   attr_accessor :depature, :destination, :depature_at
+
+  def self.ensure(depature, destination)
+    lt = self.where(destination: destination, depature: depature).first
+    return lt if lt.present?
+
+    client = SpiderWeb::HtmlClient.new
+    lt2 = client.get_last_routes(destination, depature)[0]
+    return LastTrain.create(depature: depature, destination: destination, depature_at: lt2.limit_time)
+  end
 end
--- test/models/last_train_test.rb
+++ test/models/last_train_test.rb
@@ -3,5 +3,11 @@
 require 'test_helper'
 
 class LastTrainTest < ActiveSupport::TestCase
+  test "ensure" do
+    lt = LastTrain.ensure("溜池山王", "新宿")
+    assert_equal lt.depature, "溜池山王"
+    assert_equal lt.destination, "新宿"
+    assert_not_nil lt.depature_at
+  end
 
 end
